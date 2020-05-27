76a5e19597a2a4445a3e6af1f66344e2f1dc6062
Michael Takeuchi <miketaki@sbcglobal.net>
2020-05-27 00:19:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,9 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each_with_index
+  result = []
+  array.each_with_index do |ingredient|
+    result << ingredient if cheese_types.include?(ingredient)
+  end
+  result
 end
